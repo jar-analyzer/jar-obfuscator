a5b1584ef4151a3c641b977c8fc631755aa92d14
4ra1n <2023503307@qq.com>
2024-05-23 15:05:53 +0800

    RELEASE

--- src/main/java/me/n1ar4/jar/obfuscator/Const.java
+++ src/main/java/me/n1ar4/jar/obfuscator/Const.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public interface Const {
-    String VERSION = "0.0.2-beta";
+    String VERSION = "0.0.3-beta";
     String PROJECT_URL = "https://github.com/jar-analyzer/jar-obfuscator";
     String TEMP_DIR = "jar-obfuscator-temp";
     Path configPath = Paths.get("config.yaml");
