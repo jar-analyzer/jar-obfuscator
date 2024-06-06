9d9211995c2dd34c1d298e572356c2f37a844f26
4ra1n <2023503307@qq.com>
2024-06-06 13:27:25 +0800

    [功能] 启动时请求 `OSS` 检测是否有新版本可用

--- src/main/java/me/n1ar4/jar/obfuscator/Const.java
+++ src/main/java/me/n1ar4/jar/obfuscator/Const.java
@@ -8,6 +8,8 @@ import java.nio.file.Paths;
 public interface Const {
     String VERSION = "0.0.5-beta";
     String PROJECT_URL = "https://github.com/jar-analyzer/jar-obfuscator";
+    String UPDATE_URL = "https://jar-analyzer.oss-cn-hangzhou.aliyuncs.com/jar-obfuscator/version.txt";
+    String DOWNLOAD_URL = "https://github.com/jar-analyzer/jar-obfuscator/releases/latest";
     String TEMP_DIR = "jar-obfuscator-temp";
     Path configPath = Paths.get("config.yaml");
     int ASMVersion = Opcodes.ASM9;
--- src/main/java/me/n1ar4/jar/obfuscator/Logo.java
+++ src/main/java/me/n1ar4/jar/obfuscator/Logo.java
@@ -1,5 +1,7 @@
 package me.n1ar4.jar.obfuscator;
 
+import me.n1ar4.jar.obfuscator.utils.UpdateChecker;
+
 public class Logo {
     public static void printLogo() {
         System.out.println("     ____.________ ___.    _____                           __                \n" +
@@ -10,9 +12,13 @@ public class Logo {
                 "                  \\/    \\/                \\/     \\/     \\/                   ");
         System.out.println("Jar Obfuscator - An Open-Source Java Bytecode Obfuscation Tool");
         System.out.println("Version: " + Const.VERSION + " URL: " + Const.PROJECT_URL + "\n");
+
         System.out.println("注意：当前版本的方法名混淆 enableMethodName 不稳定可能报错\n" +
                 "建议暂时关闭 enableMethodName 配置替换为组合使用其他各种混淆搭配");
-        System.out.println("感谢使用！有问题和建议欢迎在 GITHUB ISSUE 反馈");
+        System.out.println("感谢使用！有问题和建议欢迎在 GITHUB ISSUE 反馈\n");
+
+        UpdateChecker.check();
+
         System.out.println();
     }
 }
