140c3d697943913386dba085b749f5285cf7e0a1
4ra1n <2023503307@qq.com>
2024-06-07 17:06:36 +0800

    [功能] 记录贡献者列表并在启动时打印感谢信息

--- src/main/java/me/n1ar4/jar/obfuscator/Logo.java
+++ src/main/java/me/n1ar4/jar/obfuscator/Logo.java
@@ -1,19 +1,45 @@
 package me.n1ar4.jar.obfuscator;
 
 import me.n1ar4.jar.obfuscator.utils.ColorUtil;
+import me.n1ar4.jar.obfuscator.utils.IOUtils;
 import me.n1ar4.jar.obfuscator.utils.UpdateChecker;
+
+import java.io.InputStream;
 
 public class Logo {
     public static void printLogo() {
-        System.out.println(ColorUtil.green("     ____.________ ___.    _____                           __                \n" +
-                "    |    |\\_____  \\\\_ |___/ ____\\_ __  ______ ____ _____ _/  |_  ___________ \n" +
-                "    |    | /   |   \\| __ \\   __\\  |  \\/  ___// ___\\\\__  \\\\   __\\/  _ \\_  __ \\\n" +
-                "/\\__|    |/    |    \\ \\_\\ \\  | |  |  /\\___ \\\\  \\___ / __ \\|  | (  <_> )  | \\/\n" +
-                "\\________|\\_______  /___  /__| |____//____  >\\___  >____  /__|  \\____/|__|   \n" +
-                "                  \\/    \\/                \\/     \\/     \\/                   "));
+        System.out.println(ColorUtil.green(
+                "     ____.________ ___.    _____                           __                \n" +
+                        "    |    |\\_____  \\\\_ |___/ ____\\_ __  ______ ____ _____ _/  |_  ___________ \n" +
+                        "    |    | /   |   \\| __ \\   __\\  |  \\/  ___// ___\\\\__  \\\\   __\\/  _ \\_  __ \\\n" +
+                        "/\\__|    |/    |    \\ \\_\\ \\  | |  |  /\\___ \\\\  \\___ / __ \\|  | (  <_> )  | \\/\n" +
+                        "\\________|\\_______  /___  /__| |____//____  >\\___  >____  /__|  \\____/|__|   \n" +
+                        "                  \\/    \\/                \\/     \\/     \\/                   "));
         System.out.println(ColorUtil.blue("Jar Obfuscator - An Open-Source Java Bytecode Obfuscation Tool"));
         System.out.println("Version: " + ColorUtil.red(Const.VERSION) +
                 " URL: " + ColorUtil.red(Const.PROJECT_URL) + "\n");
+
+        InputStream is = Logo.class.getClassLoader().getResourceAsStream("thanks.txt");
+        if (is != null) {
+            try {
+                byte[] data = IOUtils.readAllBytes(is);
+                String a = new String(data);
+                String[] splits = a.split("\n");
+                if (splits.length > 1) {
+                    System.out.println(ColorUtil.green("感谢以下用户对本项目的贡献:"));
+                }
+                for (String s : splits) {
+                    if (s.endsWith("\r")) {
+                        s = s.substring(0, s.length() - 1);
+                    }
+                    String[] temp = s.split(" ");
+                    System.out.println(" -> " + ColorUtil.blue(temp[0]) +
+                            " " + ColorUtil.yellow(temp[1]));
+                }
+                System.out.println();
+            } catch (Exception ignored) {
+            }
+        }
 
         System.out.println("注意：方法名混淆 " + ColorUtil.yellow("enableMethodName") + " 需要考虑多种情况\n" +
                 "例如混淆了某个 " + ColorUtil.yellow("@Override") + " 的方法名会导致方法无法正常执行\n" +
