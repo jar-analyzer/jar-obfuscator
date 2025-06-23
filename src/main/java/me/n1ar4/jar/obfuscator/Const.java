7ce5976c0d0a0775c609188974bcef67249515e8
4ra1n <2023503307@qq.com>
2025-06-24 00:04:13 +0800

    release 2.0.0-RC3

--- src/main/java/me/n1ar4/jar/obfuscator/Const.java
+++ src/main/java/me/n1ar4/jar/obfuscator/Const.java
@@ -1,3 +1,15 @@
+/*
+ * MIT License
+ *
+ * Project URL: https://github.com/jar-analyzer/jar-obfuscator
+ *
+ * Copyright (c) 2024-2025 4ra1n (https://github.com/4ra1n)
+ *
+ * This project is distributed under the MIT license.
+ *
+ * https://opensource.org/license/mit
+ */
+
 package me.n1ar4.jar.obfuscator;
 
 import org.objectweb.asm.ClassReader;
@@ -8,7 +20,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public interface Const {
-    String VERSION = "2.0.0-RC2";
+    String VERSION = "2.0.0-RC3";
     String PROJECT_URL = "https://github.com/jar-analyzer/jar-obfuscator";
     String TEMP_DIR = "jar-obfuscator-temp";
     Path configPath = Paths.get("config.yaml");
--- src/main/java/me/n1ar4/jar/obfuscator/config/Parser.java
+++ src/main/java/me/n1ar4/jar/obfuscator/config/Parser.java
@@ -1,3 +1,15 @@
+/*
+ * MIT License
+ *
+ * Project URL: https://github.com/jar-analyzer/jar-obfuscator
+ *
+ * Copyright (c) 2024-2025 4ra1n (https://github.com/4ra1n)
+ *
+ * This project is distributed under the MIT license.
+ *
+ * https://opensource.org/license/mit
+ */
+
 package me.n1ar4.jar.obfuscator.config;
 
 import me.n1ar4.jar.obfuscator.Const;
